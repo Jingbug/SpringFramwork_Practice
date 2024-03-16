a00a73d254f695602f8a99fd12d5f807ea466e63
Jingles <rjy747@gmail.com>
2024-03-16 15:56:45 +0900

    Strating Position Practice

--- src/main/java/com/example/practice/AutoAppConfig.java
+++ src/main/java/com/example/practice/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // 수동으로 Bean을 동록하는 AppConfig.class를 제외하는 Filter 추가
+        basePackages = "com.example.practice.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
