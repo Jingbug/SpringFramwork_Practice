c72d69b5221284caf27e6a9cbe7a1157b6aac465
Jingles <rjy747@gmail.com>
2024-03-16 16:46:32 +0900

    Filter

--- src/main/java/com/example/practice/AutoAppConfig.java
+++ src/main/java/com/example/practice/AutoAppConfig.java
@@ -10,6 +10,4 @@ import org.springframework.context.annotation.FilterType;
         basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig {
-
-}
+public class AutoAppConfig { }
---
+++ src/test/java/com/example/practice/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.example.practice.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
